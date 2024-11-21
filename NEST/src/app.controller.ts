f406fb7e1879b70f2483af28728093e68b96ff3c
Karielly <kariellyycarvalho@gmail.com>
2024-11-21 13:53:37 -0300

    feat: implementar CRUD para notas

--- NEST/src/app.controller.ts
+++ NEST/src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Nota } from './types/nota';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('notas')
+  getNotas(): Nota[] {
+    return this.appService.getNotas();
+  }
+
+  @Get('notas/:id')
+  getNota(): string {
+    return this.appService.getNota();
+  }
+
+  @Post('notas')
+  createNota(@Body() nota: Nota): string {
+    return this.appService.createNota(nota);
+  }
+
+  @Put('notas/:id')
+  updateNota(
+    @Param('id') id: number,
+    @Body('titulo') titulo: string,
+    @Body('texto') texto: string,
+  ): string {
+    return this.appService.updateNota(id, titulo, texto);
+  }
+
+  @Delete('notas/:id')
+  deleteNota(@Param('id') id: number): string {
+    return this.appService.deleteNota(id);
   }
 }
--- NEST/src/app.service.ts
+++ NEST/src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Nota } from './types/nota';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  notas: Nota[] = [];
+
+  getNotas(): Nota[] {
+    return this.notas;
+  }
+
+  getNota(): string {
+    // buscar uma nota e retornar
+    return 'Retornar nota';
+  }
+
+  createNota(nota: Nota): string {
+    this.notas.push(nota);
+    // console.log(this.notas);
+    return 'Nota criada com sucesso';
+  }
+
+  updateNota(id: number, titulo: string, texto: string): string {
+    // atualizar uma nota pelo id da nota
+    for (let i = 0; i < this.notas.length; i++) {
+      if (this.notas[i].id == id) {
+        this.notas[i].titulo = titulo;
+        this.notas[i].texto = texto;
+        return `Nota atualizada com sucesso ${id}`;
+      }
+    }
+    console.log(this.notas);
+  }
+
+  deleteNota(id: number): string {
+    console.log(id);
+    for (let i = 0; i < this.notas.length; i++) {
+      if (this.notas[i].id == id) {
+        this.notas.splice(i, 1);
+        return `Nota deletada com sucesso ${id}`;
+      }
+    }
+    console.log(this.notas);
+    return `Nota com ID ${id} não encontrada`;
   }
 }
